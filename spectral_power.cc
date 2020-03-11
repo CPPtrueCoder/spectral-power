5899e51fb1f60bdef983752f0817c9512fdfa42f
CPPtrueCoder <andierodin@gmail.com>
2020-03-12 01:22:34 +0300

    First commit

---
+++ spectral_power.cc
@@ -0,0 +1,5 @@
+//
+// Created by andie on 11.03.2020.
+//
+
+#include "spectral_power.h"
---
+++ spectral_power.h
@@ -0,0 +1,13 @@
+//
+// Created by andie on 11.03.2020.
+//
+
+#ifndef SPECTRAL_POWER_SPECTRAL_POWER_H
+#define SPECTRAL_POWER_SPECTRAL_POWER_H
+
+class Spectral_power {
+    void getFunction();
+
+};
+
+#endif //SPECTRAL_POWER_SPECTRAL_POWER_H
